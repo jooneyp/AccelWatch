ce3374715e2de22c8b3239a91be2ec359deb8604
Park Joonyoung <parkjydev@gmail.com>
2016-04-21 15:47:50 +0900

    Initial commit 2

--- AccelWatch WatchKit Extension/InterfaceController.swift
+++ AccelWatch WatchKit Extension/InterfaceController.swift
@@ -7,25 +7,170 @@
 //
 
 import WatchKit
+import CoreMotion
 import Foundation
+import WatchConnectivity
 
 
-class InterfaceController: WKInterfaceController {
+class InterfaceController: WKInterfaceController, WCSessionDelegate {
+    @IBOutlet var labelTime: WKInterfaceLabel!
+    @IBOutlet var labelStatus: WKInterfaceLabel!
+    @IBOutlet var btnSend: WKInterfaceButton!
+    @IBOutlet var btnStart: WKInterfaceButton!
+    
+    let motionManager = CMMotionManager()
+    var accDataX = [String]()
+    var accDataY = [String]()
+    var accDataZ = [String]()
+    var timeData = [String]()
+    
+    var min = 0
+    var sec = 0
+    var flag = 0
+    var stamp = 0.0
+    
+    var recStatus: Bool!
+    var session : WCSession!
 
     override func awakeWithContext(context: AnyObject?) {
         super.awakeWithContext(context)
-        
         // Configure interface objects here.
+        
+        if (WCSession.isSupported()) {
+            let session = WCSession.defaultSession()
+            session.delegate = self // conforms to WCSessionDelegate
+            session.activateSession()
+        }
+        motionManager.accelerometerUpdateInterval = 0.01
+        self.labelStatus.setText(String(format: "Stopped"))
+        recStatus = false
     }
 
     override func willActivate() {
         // This method is called when watch view controller is about to be visible to user
         super.willActivate()
+        btnSend.setHidden(true)
+    
+        if (motionManager.accelerometerAvailable == true) {
+            let handler:CMAccelerometerHandler = {(data: CMAccelerometerData?, error: NSError?) -> Void in
+                if (self.recStatus == true) {
+                    self.stamp += 0.01
+                    self.flag += 1
+                    self.timeData.append(String(format: "%.2f", self.stamp))
+                    self.accDataX.append(String(format: "%.3f", data!.acceleration.x))
+                    self.accDataY.append(String(format: "%.3f", data!.acceleration.y))
+                    self.accDataZ.append(String(format: "%.3f", data!.acceleration.z))
+                    if self.flag % 100 == 0 {
+                        self.sec += 1
+                        self.labelTime.setText(String(format: "%02d:%02d", self.min, self.sec))
+                        if self.sec % 59 == 0 {
+                            self.min += 1
+                            self.sec = 0
+                        } else {
+                        }
+                    }
+                } else {
+                }
+            }
+            motionManager.startAccelerometerUpdatesToQueue(NSOperationQueue.currentQueue()!, withHandler: handler)
+        }
+        else {
+            self.labelStatus.setText("N/A")
+            btnSend.setHidden(true)
+            btnStart.setHidden(true)
+        }
+        
+        if(WCSession.isSupported()) {
+            session = WCSession.defaultSession()
+            session.delegate = self
+            session.activateSession()
+        }
     }
 
     override func didDeactivate() {
         // This method is called when watch view controller is no longer visible
         super.didDeactivate()
     }
+    
+    @IBAction func startBtnTapped() {
+        if self.recStatus == false {
+            self.recStatus = true
+            labelStatus.setText("Recording")
+            btnStart.setTitle("Pause")
+            btnSend.setHidden(true)
+        } else {
+            self.recStatus = false
+            labelStatus.setText("Paused")
+            btnStart.setTitle("Resume")
+            btnSend.setHidden(false)
+        }
+    }
+    
+    @IBAction func sendBtnTapped() {
+        if(WCSession.isSupported()) {
+            if self.stamp < 60.0 {
+                let appAccData = ["accData":[timeData, accDataX, accDataY, accDataZ]]
+                
+                session.sendMessage(appAccData, replyHandler: {(reply: [String : AnyObject]) -> Void in
+                        print(reply)
+                    }, errorHandler: {(error) -> Void in
+                        print(error)
+                })
+            } else {
+                var t_timeData: [String] = []
+                var t_accDataX: [String] = []
+                var t_accDataY: [String] = []
+                var t_accDataZ: [String] = []
+                for i in 0 ..< (flag / 6000) + 1 {
+                    let j = Int(i * 6000)
+                    if j+6000 > flag {
+                        t_timeData += timeData[j..<flag]
+                        t_accDataX += accDataX[j..<flag]
+                        t_accDataY += accDataY[j..<flag]
+                        t_accDataZ += accDataZ[j..<flag]
+                    } else {
+                        t_timeData += timeData[j..<j+6000]
+                        t_accDataX += accDataX[j..<j+6000]
+                        t_accDataY += accDataY[j..<j+6000]
+                        t_accDataZ += accDataZ[j..<j+6000]
+                    }
+                    var appAccData = ["accData":[t_timeData, t_accDataX, t_accDataY, t_accDataZ]]
+                    session.sendMessage(appAccData, replyHandler: {(reply: [String : AnyObject]) -> Void in
+                        print(reply)
+                        }, errorHandler: {(error) -> Void in
+                            print(error)
+                    })
+                    NSThread.sleepForTimeInterval(1)
+                    t_timeData.removeAll()
+                    t_accDataX.removeAll()
+                    t_accDataY.removeAll()
+                    t_accDataZ.removeAll()
+                    appAccData.removeAll()
+                }
+            }
+        }
+        self.timeData.removeAll()
+        self.accDataX.removeAll()
+        self.accDataY.removeAll()
+        self.accDataZ.removeAll()
+        labelStatus.setText("Stopped")
+        btnStart.setTitle("Start")
+        labelTime.setText("00:00")
+        btnSend.setHidden(true)
+        self.stamp = 0.0
+        self.min = 0
+        self.sec = 0
+        self.flag = 0
+        self.recStatus = false
+    }
 
+}
+
+extension Array {
+    func splitBy(subSize: Int) -> [[Element]] {
+        return 0.stride(to: self.count, by: subSize).map { startIndex in
+            let endIndex = startIndex.advancedBy(subSize, limit: self.count)
+            return Array(self[startIndex ..< endIndex])
+        }
+    }
 }
--- AccelWatch/ViewController.swift
+++ AccelWatch/ViewController.swift
@@ -7,19 +7,165 @@
 //
 
 import UIKit
+import WatchConnectivity
+import CoreData
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, WCSessionDelegate, UITableViewDataSource, UITableViewDelegate {
+    @IBOutlet var mainTableView: UITableView!
+    var tableData = [String]()
+    var secondData = [String]()
+    var session: WCSession!
+    
+    var documentUbi: MyDocument?
+    var documentLocal: MyDocument?
+    var documentURL: NSURL?
+    var ubiquityURL: NSURL?
+    var metaDataQuery: NSMetadataQuery?
+    
+    var currentTime: String!
+    
+//    var document: MyDocument?
 
     override func viewDidLoad() {
         super.viewDidLoad()
+        
         // Do any additional setup after loading the view, typically from a nib.
+        
+        
+        if (WCSession.isSupported()) {
+            session = WCSession.defaultSession()
+            session.delegate = self;
+            session.activateSession()
+        }
+        self.mainTableView.reloadData()
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    func session(session: WCSession, didReceiveMessage message: [String : AnyObject], replyHandler: ([String : AnyObject]) -> Void) {
+        //handle received message
+        let value = message["accData"] as! NSArray
+        let CSVData = dataToCSV(value)
+        
+        // Generate timestamp
+        let date = NSDate()
+        let formatter = NSDateFormatter()
+        formatter.dateFormat = "yyyyMMdd_HH:mm:ss"
+        let currentTime = formatter.stringFromDate(date)
+        
+        let filemgr = NSFileManager.defaultManager()
+        
+        let dirPaths = NSSearchPathForDirectoriesInDomains(.DocumentDirectory, .UserDomainMask, true)
+        let dataFilePath = dirPaths[0].stringByAppendingString("/" + currentTime + ".csv")
+        
+        documentURL = NSURL(fileURLWithPath: dataFilePath)
+        documentLocal = MyDocument(fileURL: documentURL!)
+        documentLocal!.csvData = CSVData
+        
+        ubiquityURL = filemgr.URLForUbiquityContainerIdentifier(nil)!.URLByAppendingPathComponent("Documents").URLByAppendingPathComponent(currentTime + ".csv")
+        documentUbi = MyDocument(fileURL: ubiquityURL!)
+        documentUbi!.csvData = CSVData
+        
+        if !filemgr.fileExistsAtPath(dataFilePath) {
+            documentUbi?.saveToURL(ubiquityURL!,
+                                forSaveOperation: .ForCreating,
+                                completionHandler: {(success: Bool) -> Void in
+                                    if success {
+                                        print("iCloud create OK")
+                                    } else {
+                                        print("iCloud create failed")
+                                    }
+            })
+            documentLocal?.saveToURL(documentURL!,
+                                forSaveOperation: .ForCreating,
+                                completionHandler: {(success: Bool) -> Void in
+                                    if success {
+                                        print("Local file create OK")
+                                    } else {
+                                        print("Local file create failed")
+                                    }
+            })
+        } else {
+            print("File Exists ERROR!")
+        }
+        
+        let secondStart : Float = NSString(string: value[0].firstObject as! String).floatValue
+        let secondEnd : Float = NSString(string: value[0].lastObject as! String).floatValue
+        let dataSize = (secondEnd - secondStart) * 2.534
+        
+        dispatch_async(dispatch_get_main_queue()) {
+            self.tableData.append(currentTime)
+            self.secondData.append(String(format: "%.2f ~ %.2f", secondStart, secondEnd) + "secs / " + String(format: "%.2f", dataSize) + "KB")
+            self.mainTableView.reloadData()
+        }
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section:Int) -> Int {
+        return tableData.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell:UITableViewCell = UITableViewCell(style: UITableViewCellStyle.Subtitle, reuseIdentifier: "Default")
+        cell.textLabel!.text = tableData[indexPath.row]
+        cell.detailTextLabel!.text = secondData[indexPath.row]
+        return cell
+    }
+    
+    func dataToCSV(input: NSArray) -> String {
+        var joinedData = ""
+        
+        for i in 0 ..< 4 {
+            joinedData += input[i].componentsJoinedByString(",")
+            joinedData += "\n"
+        }
+        
+        let strings = joinedData.componentsSeparatedByString("\n").map { (string) -> [String] in string.componentsSeparatedByString(",")
+        }
+        
+        let tData = transpose(strings)
+        var transposedData = "time,X,Y,Z\n"
+        
+        for i in 0 ..< tData.count {
+            for j in 0 ..< tData[i].count {
+                transposedData += String(tData[i][j])
+                if j < 3 {
+                    transposedData += String(",")
+                }
+            }
+            transposedData += String("\n")
+        }
+        return transposedData
+    }
+    
+    func transpose<T>(input: [[T]]) -> [[T]] {
+        if input.isEmpty { return [[T]]() }
+        let count = input[0].count
+        var out = [[T]](count: count, repeatedValue: [T]())
+        for outer in input {
+            for (index, inner) in outer.enumerate() {
+                out[index].append(inner)
+            }
+        }
+        return out
+    }
 }
 
+class MyDocument: UIDocument {
+    var document: MyDocument?
+    var documentURL: NSURL?
+    var ubiquityURL: NSURL?
+    var metaDataQuery: NSMetadataQuery?
+    var csvData: String?
+    
+    override func contentsForType(typeName: String) throws -> AnyObject {
+        if let content = csvData {
+            let length = content.lengthOfBytesUsingEncoding(NSUTF8StringEncoding)
+            return NSData(bytes: content, length: length)
+        } else {
+            return NSData()
+        }
+    }
+}
